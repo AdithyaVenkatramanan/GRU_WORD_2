038c7b3632fc163896554a57c8aedbec35193c34
AdithyaVenkatramanan <93856410+AdithyaVenkatramanan@users.noreply.github.com>
2025-06-07 00:01:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+##Data preprocessing
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+import streamlit as st
+import pickle
+
+
+model= load_model('next_word_gru.h5')
+
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+
+
+def predict_next_word(model,tokenizer,text,max_sequence_len):
+    token_list=tokenizer.texts_to_sequences([text])[0]
+    if len(token_list) >= max_sequence_len:
+        token_list = token_list[-(max_sequence_len-1):]
+
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+    predicted = model.predict(token_list, verbose=0)
+    predicted_word_index=np.argmax(predicted[0])
+    for word,index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None
+
+
+
+#streamlit app
+st.title("Next Word Prediction using GRU")
+st.write("Enter a sentence to predict the next word:")
+
+input_text = st.text_input("Input Text","to be or not to be")
+
+if st.button("Predict Next Word"):
+    max_sequence_len=model.input_shape[1]+1
+    next_word=predict_next_word(model,tokenizer,input_text,max_sequence_len)
+    st.write(f"next word:{next_word}")
+
+
